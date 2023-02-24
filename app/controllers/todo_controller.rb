a707ba67a1adfd5c4a359c428806be4420efecba
joy <letimjoy7@gmail.com>
2023-02-24 11:23:44 +0300

    done

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,7 +1,7 @@
-#a class that inherits the sinatra base
+#a class that inherits the sinatra::base class and defines a single route using the get method
 class TodoController < Sinatra::Base
 
-    get '/' do
+    get '/' do#root path of the application , the block code within the get will be executed
         "Our very first sinatra"
     end
 
--- app/models/todos.rb
+++ app/models/todos.rb
@@ -1,2 +1,3 @@
+#class inherits from the ActiveRecord::base class which provides it with a number of methods for interacting with a database.
 class Todo < ActiveRecord::Base
 end
